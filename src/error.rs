76dc56114c26e3d9c5cc1c78d3a8e1c4add30f85
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-11-09 22:46:31 +0300

    refactor(deps): switch to `thiserror` for error handling

--- src/error.rs
+++ src/error.rs
@@ -1,45 +1,45 @@
 #![allow(missing_docs)]
 
-thiserror_lite::err_enum! {
-    #[derive(Debug)]
-    pub enum Error {
-        #[error("IO error: `{}`")]
-        Io(#[from] std::io::Error),
-        #[error("D-Bus error: `{}`")]
-        Dbus(#[from] dbus::Error),
-        #[error("D-Bus string error: `{}`")]
-        DbusString(String),
-        #[error("D-Bus argument error: `{}`")]
-        DbusArgument(String),
-        #[error("X11 connect error: `{}`")]
-        X11Connect(#[from] x11rb::errors::ConnectError),
-        #[error("X11 connection error: `{}`")]
-        X11Connection(#[from] x11rb::errors::ConnectionError),
-        #[error("X11 ID error: `{}`")]
-        X11Id(#[from] x11rb::errors::ReplyOrIdError),
-        #[error("X11 error: `{}`")]
-        X11Other(String),
-        #[error("Cairo error: `{}`")]
-        Cairo(#[from] cairo::Error),
-        #[error("Pango error: `{}`")]
-        PangoOther(String),
-        #[error("Receiver error: `{}`")]
-        Receiver(#[from] std::sync::mpsc::RecvError),
-        #[error("TOML parsing error: `{}`")]
-        Toml(#[from] toml::de::Error),
-        #[error("Scan error: `{}`")]
-        Scanf(String),
-        #[error("Integer conversion error: `{}`")]
-        IntegerConversion(#[from] std::num::TryFromIntError),
-        #[error("Template error: `{}`")]
-        Template(#[from] tera::Error),
-        #[error("Template parse error:\n{}")]
-        TemplateParse(String),
-        #[error("Template render error:\n{}")]
-        TemplateRender(String),
-        #[error("Config error: `{}`")]
-        Config(String),
-    }
+use thiserror::Error as ThisError;
+
+#[derive(Debug, ThisError)]
+pub enum Error {
+    #[error("IO error: `{0}`")]
+    Io(#[from] std::io::Error),
+    #[error("D-Bus error: `{0}`")]
+    Dbus(#[from] dbus::Error),
+    #[error("D-Bus string error: `{0}`")]
+    DbusString(String),
+    #[error("D-Bus argument error: `{0}`")]
+    DbusArgument(String),
+    #[error("X11 connect error: `{0}`")]
+    X11Connect(#[from] x11rb::errors::ConnectError),
+    #[error("X11 connection error: `{0}`")]
+    X11Connection(#[from] x11rb::errors::ConnectionError),
+    #[error("X11 ID error: `{0}`")]
+    X11Id(#[from] x11rb::errors::ReplyOrIdError),
+    #[error("X11 error: `{0}`")]
+    X11Other(String),
+    #[error("Cairo error: `{0}`")]
+    Cairo(#[from] cairo::Error),
+    #[error("Pango error: `{0}`")]
+    PangoOther(String),
+    #[error("Receiver error: `{0}`")]
+    Receiver(#[from] std::sync::mpsc::RecvError),
+    #[error("TOML parsing error: `{0}`")]
+    Toml(#[from] toml::de::Error),
+    #[error("Scan error: `{0}`")]
+    Scanf(String),
+    #[error("Integer conversion error: `{0}`")]
+    IntegerConversion(#[from] std::num::TryFromIntError),
+    #[error("Template error: `{0}`")]
+    Template(#[from] tera::Error),
+    #[error("Template parse error:\n{0}")]
+    TemplateParse(String),
+    #[error("Template render error:\n{0}")]
+    TemplateRender(String),
+    #[error("Config error: `{0}`")]
+    Config(String),
 }
 
 /// Type alias for the standard [`Result`] type.

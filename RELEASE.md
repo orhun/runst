d074bdedec5e4ceb0f1a075f058b1cbaf49cf6cd
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-03-02 21:11:01 +0300

    chore(release): add release instructions

---
+++ RELEASE.md
@@ -0,0 +1,9 @@
+# Creating a Release
+
+[GitHub](https://github.com/orhun/runst/releases) and [crates.io](https://crates.io/crates/runst) releases are automated via [GitHub actions](.github/workflows/cd.yml) and triggered by pushing a tag.
+
+1. Run the [release script](./release.sh): `./release.sh v[X.Y.Z]`
+2. Push the changes: `git push`
+3. Check if [Continuous Integration](https://github.com/orhun/runst/actions) workflow is completed successfully.
+4. Push the tags: `git push --tags`
+5. Wait for [Continuous Deployment](https://github.com/orhun/runst/actions) workflow to finish.
---
+++ release.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ]; then
+	echo "Please provide a tag."
+	echo "Usage: ./release.sh v[X.Y.Z]"
+	exit
+fi
+
+echo "Preparing $1..."
+# update the version
+msg="# bumped by release.sh"
+sed -E -i "s/^version = .* $msg$/version = \"${1#v}\" $msg/" Cargo.toml
+cargo build --locked
+# update the changelog
+git cliff --tag "$1" >CHANGELOG.md
+git add -A
+git commit -m "chore(release): prepare for $1"
+git show
+# generate a changelog for the tag message
+changelog=$(git cliff --tag "$1" --unreleased --strip all)
+# create a signed tag
+# https://keyserver.ubuntu.com/pks/lookup?search=0x1B250A9F78535D1A&op=vindex
+git -c user.name="runst" \
+	-c user.email="runstify@proton.me" \
+	-c user.signingkey="AEF8C7261F4CEB41A448CBC41B250A9F78535D1A" \
+	tag -f -s -a "$1" -m "Release $1" -m "$changelog"
+git tag -v "$1"
+echo "Done!"
+echo "Now push the commit (git push) and the tag (git push --tags)."
